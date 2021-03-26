b4f929f4fae7ff9b3f94a7f40b6c7fd8036f52f4
DmitryGla <81014874+DmitryGla@users.noreply.github.com>
2021-03-26 15:50:53 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,6 +3,6 @@ link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_user_should_see_button(browser):
     browser.get(link)
-    button = browser.find_element_by_css_selector("[type='submit']")
+    button = browser.find_element_by_css_selector(".btn-add-to-basket")
     assert button, "No button"
     #time.sleep(10)
